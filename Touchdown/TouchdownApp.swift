e9ce1073afd3e51a5822bc6bc2509f908a74bad4
zlatkoiliev <83271119+zlatkoiliev@users.noreply.github.com>
2021-07-10 15:01:27 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Zlatko Iliev on 10.07.21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
